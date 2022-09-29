2007c183e05ee5437d52d91d8723a47d010e7008
damian <damiangalvan66@gmail.com>
2022-09-29 10:00:28 -0500

    refactor, WithMockUser annotation added

--- src/test/java/rest/api/example/user/UserControllerTest.java
+++ src/test/java/rest/api/example/user/UserControllerTest.java
@@ -1,24 +1,19 @@
 package rest.api.example.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import rest.api.example.shared.BaseControllerTest;
 import rest.api.example.user.entities.User;
 
 import java.util.Optional;
 
 import static org.mockito.BDDMockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,39 +24,24 @@ class UserControllerTest extends BaseControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
-    SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor principalUser;
-
-    ObjectMapper mapper;
-
-    @BeforeEach
-    void setUp() {
-        principalUser = user("damian@gmail.com").password("pass").roles("USER");
-        mapper = new ObjectMapper();
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-    }
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Test
     @DisplayName("should return 200 ok")
+    @WithMockUser(roles = "USER", username = "damian@gmail.com")
     void shouldGetAllUsers() throws Exception {
-        mvc.perform(get("/api/1.0/users").with(principalUser))
+        mvc.perform(get("/api/1.0/users"))
                 .andExpect(status().isOk());
-
     }
 
     @Test
     @DisplayName("should return 200 ok when user exists")
+    @WithMockUser(roles = "USER", username = "damian@gmail.com")
     void shouldGetUserById() throws Exception {
         User user = new User("damian", "damian@gmail.com", "123456");
         given(userService.findUserById(1L)).willReturn(Optional.of(user));
 
-        mvc.perform(get("/api/1.0/users/1").with(principalUser))
-                .andExpect(status().isOk())
+        mvc.perform(get("/api/1.0/users/1")).andExpect(status().isOk())
                 .andExpect(jsonPath("$.username").value("damian"))
                 .andExpect(jsonPath("$.email").value("damian@gmail.com"));
 
@@ -70,19 +50,20 @@ class UserControllerTest extends BaseControllerTest {
 
     @Test
     @DisplayName("should return 404 when user does not exist")
+    @WithMockUser(roles = "USER", username = "damian@gmail.com")
     void shouldNotGetUserById() throws Exception {
         given(userService.findUserById(1L)).willReturn(Optional.empty());
 
-        mvc.perform(get("/api/1.0/users/1").with(principalUser))
+        mvc.perform(get("/api/1.0/users/1"))
                 .andExpect(status().isNotFound());
-
     }
 
     @Test
     @DisplayName("should return 200 when the user is successfully deleted")
+    @WithMockUser(roles = "USER", username = "damian@gmail.com")
     void shouldDeleteUserById() throws Exception {
 
-        mvc.perform(delete("/api/1.0/users/1").with(principalUser))
+        mvc.perform(delete("/api/1.0/users/1"))
                 .andExpect(status().isOk());
 
         then(userService).should().deleteUserById(1L);
@@ -91,12 +72,13 @@ class UserControllerTest extends BaseControllerTest {
 
     @Test
     @DisplayName("should return 200 when the user is successfully updated")
+    @WithMockUser(roles = "USER", username = "damian@gmail.com")
     void shouldUpdateUser() throws Exception {
         User userRequest = new User("username updated", "damian222@gmail.com", "2233232");
 
         given(userService.updateUser(1L, userRequest)).willReturn(userRequest);
 
-        mvc.perform(put("/api/1.0/users/1").with(principalUser)
+        mvc.perform(put("/api/1.0/users/1")
                         .content(mapper.writeValueAsString(userRequest))
                         .contentType(MediaType.APPLICATION_JSON)
                 ).andExpect(status().isOk())
