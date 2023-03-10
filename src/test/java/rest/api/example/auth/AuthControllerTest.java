705b0437a4e140a89e315dbd0469161ef3b45dc4
damian <damiangalvan66@gmail.com>
2023-03-10 13:31:00 -0600

    fix: tests are not loading Spring security resolved
    Link to the resource that helped resolve the bug: https://stackoverflow.com/questions/72603582/include-security-filters-when-running-controller-tests

--- src/test/java/rest/api/example/auth/AuthControllerTest.java
+++ src/test/java/rest/api/example/auth/AuthControllerTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
+import rest.api.example.security.SpringSecurityConfig;
 import rest.api.example.shared.BaseControllerTest;
 import rest.api.example.user.entities.User;
 
@@ -22,13 +24,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@Import(SpringSecurityConfig.class)
 class AuthControllerTest extends BaseControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @MockBean
-    UserDetailsService userDetailsService;
 
     private final ObjectMapper mapper = new ObjectMapper();
 
--- src/test/java/rest/api/example/shared/BaseControllerTest.java
+++ src/test/java/rest/api/example/shared/BaseControllerTest.java
@@ -1,16 +1,21 @@
 package rest.api.example.shared;
 
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import rest.api.example.auth.jwt.JWTService;
+import rest.api.example.security.HttpConfigurer;
 import rest.api.example.user.services.UserService;
 
 public class BaseControllerTest {
-
     @MockBean
     protected JWTService jwtService;
     @MockBean
     protected UserService userService;
     @MockBean
     protected PasswordEncoder passwordEncoder;
+    @MockBean
+    protected UserDetailsService userDetailsService;
+    @MockBean
+    protected HttpConfigurer customHttpConfigurer;
 }
